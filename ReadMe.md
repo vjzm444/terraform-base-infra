2ac8d0a7b01459869c24eb25fe8b2095e8073c43
DESKTOP-883EC3D\soldesk <vjzm44@naver.com>
2026-06-10 15:03:04 +0900

    terraform실행관련 ReadMe추가, Grafana인스턴스에서 Athena iam권한설정 추가

--- ReadMe.md
+++ ReadMe.md
@@ -6,9 +6,20 @@
 1-2. terraform plan 확인
 1-3. terraform apply 적용
 
-2-2. 쿠버네티스 ALB적용(로그그룹: /ec2/vamserlike-backend에 전송되야함)
+2-1. 쿠버네티스 ALB 띄우기...
+2-2. 쿠버네티스 ALB Log보내기(로그그룹: /ec2/vamserlike-backend에 전송되야함)
 
-2. CloudFormation으로 firehose.yml 적용하기!!!
+3. CloudFormation으로 firehose.yml 적용하기!!!
+
+4. Athena-Grafana연결 방법
+4-1. 기영 Grafana Url로 접속
+4-2. Connections -> Add new connection -> Athena install -> 이하처럼 셋팅
+- Default Region: ap-northeast-2
+- Data source: AwsDataCatalog
+- database: web_logs_db
+- database: primary
+- Output Location: s3://{너의AccountId}-vamserlike-backend-logs/athena-results/
+4-3. Dashboard메뉴 -> import -> grafana-vamserlike-backend.json 선택 -> 쿼리 최초1번 Run실행
 
 
 
--- services/instance/testiamrole.tf
+++ services/instance/testiamrole.tf
@@ -1,4 +1,6 @@
 # 1. IAM Role 생성
+
+# private_Backend_test 내부인스턴스가 사용하고있음.
 resource "aws_iam_role" "backend_role" {
   name = "backend-server-role"
 
@@ -12,15 +14,69 @@ resource "aws_iam_role" "backend_role" {
   })
 }
 
-# 2. 필요한 정책 연결 (CloudWatchLogsFullAccess + CognitoFullAccess)
+# 2. 클라우드워치 로그 권한
 resource "aws_iam_role_policy_attachment" "cw_access" {
   role       = aws_iam_role.backend_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
 
+# 3. 코그니토 권한
 resource "aws_iam_role_policy_attachment" "cognito_access" {
   role       = aws_iam_role.backend_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonCognitoPowerUser" # 또는 CognitoFullAccess
+}
+
+
+# 4. Grafana에서 아테나를 봐야되기에 관련권한(TODO: 추후 기영 Grafana인스턴스에 부착예정)
+resource "aws_iam_role_policy" "athena_access_policy" {
+  name = "athena-access-policy"
+  role = aws_iam_role.backend_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "athena:ListDataCatalogs",
+          "athena:ListDatabases",
+          "athena:ListTableMetadata",
+          "athena:GetTable",
+          "athena:StartQueryExecution",
+          "athena:GetQueryExecution",
+          "athena:GetQueryResults",
+          "athena:ListWorkGroups",
+          "athena:GetWorkGroup"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "glue:GetDatabase",
+          "glue:GetDatabases",
+          "glue:GetTable",
+          "glue:GetTables",
+          "glue:GetPartition",
+          "glue:GetPartitions"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetBucketLocation",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:PutObject"
+        ]
+        Resource = [
+          aws_s3_bucket.vamserlike-logs-bucket.arn,
+          "${aws_s3_bucket.vamserlike-logs-bucket.arn}/*"
+        ]
+      }
+    ]
+  })
 }
 
 # 3. Instance Profile 생성 (인스턴스에 붙이기 위한 필수 단계)
